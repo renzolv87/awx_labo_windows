07a937d33438964211e8e98e98df097a2f70bf3c
renzolv87 <renzo.lv87@gmail.com>
2022-04-19 17:01:49 +0200

    comento entradas que no funcionan

---
+++ playbooks/roles/win_sql/files/dump.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS test (           
+  message varchar(255) NOT NULL         
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;         
+  INSERT INTO test(message) VALUES('Esto es una prueba 1');
+  INSERT INTO test(message) VALUES('Esto es una prueba 2');         
+  INSERT INTO test(message) VALUES('Esto es una prueba 3')
